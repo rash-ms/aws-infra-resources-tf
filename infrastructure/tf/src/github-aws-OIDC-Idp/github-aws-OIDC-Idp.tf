f7b576608043e8adcaab1f908978f50b575649ae
rash-ms <rashmujeeb@yahoo.com>
2024-04-09 19:37:11 +0100

    ci fixes

--- infrastructure/tf/src/github-aws-OIDC-Idp/github-aws-OIDC-Idp.tf
+++ infrastructure/tf/src/github-aws-OIDC-Idp/github-aws-OIDC-Idp.tf
@@ -15,7 +15,7 @@ resource "aws_iam_role_policy_attachment" "github_actions_admin_access_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-data "aws_iam_policy_document" "github_assume_role_policy" {
+data "aws_iam_policy_document" "assume_role_policy" {
     statement {
       actions = ["sts:AssumedRoleWithWebIdnetity"]
       effect  = "Allow"
