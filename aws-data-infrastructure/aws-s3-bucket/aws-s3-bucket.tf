0ab443fe6b8852fdb0494e880644aaf318c95c9e
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-11 17:36:35 +0100

    create s3 bucket

--- aws-data-infrastructure/aws-s3-bucket/aws-s3-bucket.tf
+++ aws-data-infrastructure/aws-s3-bucket/aws-s3-bucket.tf
@@ -2,7 +2,7 @@ provider "aws" {
   alias  = "dev"
   region = "us-east-1"
   assume_role {
-    role_arn = "arn:aws:iam::022499035568:role/byt-internal-workspace-dev-role"
+    role_arn = "arn:aws:iam::022499035350:role/byt-internal-workspace-dev-role"
   }
 }
 
