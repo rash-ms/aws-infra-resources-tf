11c14e7dde5ede4bfbd943355e34a20dda6f7910
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-11 18:25:57 +0100

    create s3 bucket

--- aws-data-infrastructure/aws-s3-bucket/aws-s3-bucket.tf
+++ aws-data-infrastructure/aws-s3-bucket/aws-s3-bucket.tf
@@ -1,3 +1,38 @@
+provider "aws" {
+  alias  = "dev"
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::022499035350:role/byt-internal-workspace-dev-role"
+  }
+}
+
+provider "aws" {
+  alias  = "prod"
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::022499035568:role/byt-internal-workspace-prod-role"
+  }
+}
+
+
+locals {
+  bucket_config = yamldecode(file("${path.module}/aws-s3-bucket-yaml/us/us-workspace-s3-buckets.yaml"))
+}
+
+locals {
+  bucket_environment_pairs = flatten([
+    for bucket_name, bucket_data in local.bucket_config : [
+      for env in bucket_data.project-environment : {
+        bucket_name    = bucket_name
+        environment    = env
+        region         = bucket_data.region
+        team_names     = bucket_data["project-team-names"]
+      }
+    ]
+  ])
+}
+
+
 # S3 Buckets for dev environment
 resource "aws_s3_bucket" "s3_buckets_dev" {
   for_each = { for pair in local.bucket_environment_pairs : "${pair.bucket_name}-${pair.environment}" => pair if pair.environment == "dev" }
