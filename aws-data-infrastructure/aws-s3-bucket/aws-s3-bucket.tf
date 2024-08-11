34a614e5e4ff9faa7b51f640bbf4bb03e9acde7b
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-08-11 17:16:57 +0100

    create s3 bucket

--- aws-data-infrastructure/aws-s3-bucket/aws-s3-bucket.tf
+++ aws-data-infrastructure/aws-s3-bucket/aws-s3-bucket.tf
@@ -29,18 +29,29 @@ locals {
       }
     ]
   ])
+}
 
-  provider_alias = {
-    dev  = aws.dev
-    prod = aws.prod
+# S3 Buckets for dev environment
+resource "aws_s3_bucket" "s3_buckets_dev" {
+  for_each = { for pair in local.bucket_environment_pairs : "${pair.bucket_name}-${pair.environment}" => pair if pair.environment == "dev" }
+
+  provider = aws.dev
+
+  bucket = "byt-${each.value.bucket_name}-${each.value.environment}"
+  acl    = "private"
+
+  tags = {
+    Name        = "byt-${each.value.bucket_name}-${each.value.environment}"
+    Environment = each.value.environment
+    Team        = join(", ", each.value.team_names)
   }
 }
 
-# S3 Bucket resource block
-resource "aws_s3_bucket" "s3_buckets" {
-  for_each = { for pair in local.bucket_environment_pairs : "${pair.bucket_name}-${pair.environment}" => pair }
+# S3 Buckets for prod environment
+resource "aws_s3_bucket" "s3_buckets_prod" {
+  for_each = { for pair in local.bucket_environment_pairs : "${pair.bucket_name}-${pair.environment}" => pair if pair.environment == "prod" }
 
-  provider = local.provider_alias[each.value.environment]
+  provider = aws.prod
 
   bucket = "byt-${each.value.bucket_name}-${each.value.environment}"
   acl    = "private"
