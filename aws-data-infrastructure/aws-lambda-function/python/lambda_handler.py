cad63051b023fc64b97bd05ba85dc9b90724e894
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-11-06 17:00:27 +0000

    create lambda func

--- aws-data-infrastructure/aws-lambda-function/python/lambda_handler.py
+++ aws-data-infrastructure/aws-lambda-function/python/lambda_handler.py
@@ -1,7 +1,6 @@
 import json
 
 def lambda_handler(event, context):
-   message = 'Hello {} !'.format(event['key1'])
    return {
        'statusCode': 200,
        'body' : json.dumps('Hello from Lambda')
