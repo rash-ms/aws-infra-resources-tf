53a4f80f73f7be2860954e34870a1ea6351553c4
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-11-13 21:06:38 +0000

    add

--- aws-data-infrastructure/aws-lambda-function/outputs.tf
+++ aws-data-infrastructure/aws-lambda-function/outputs.tf
@@ -1,11 +1,11 @@
-# output "teraform_aws_role_output" {
-#  value = aws_iam_role.shopify_flow_api_role.name
-# }
+output "teraform_aws_role_output" {
+ value = aws_iam_role.shopify_flow_api_role.name
+}
 
-# output "teraform_aws_role_arn_output" {
-#  value = aws_iam_role.shopify_flow_api_role.arn
-# }
+output "teraform_aws_role_arn_output" {
+ value = aws_iam_role.shopify_flow_api_role.arn
+}
 
-# output "teraform_logging_arn_output" {
-#  value = aws_iam_policy.shopify_flow_iam_policy.arn
-# }
+output "teraform_logging_arn_output" {
+ value = aws_iam_policy.shopify_flow_iam_policy.arn
+}
