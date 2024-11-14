278cfaf1d938ffcaa8d601576096ce1be5d0be15
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-11-14 19:22:02 +0000

    add

--- aws-data-infrastructure/aws-apigateway-s3/outputs.tf
+++ aws-data-infrastructure/aws-apigateway-s3/outputs.tf
@@ -1,16 +1,15 @@
-# output "teraform_aws_role_output" {
-#  value = aws_iam_role.spain_sub_api_gateway_s3_api_role.name
-# }
+output "role_output" {
+ value = aws_iam_role.spain_sub_api_gateway_s3_api_role.name
+}
 
-# output "teraform_aws_role_arn_output" {
-#  value = aws_iam_role.spain_sub_api_gateway_s3_api_role.arn
-# }
+output "role_api_arn" {
+ value = aws_iam_role.spain_sub_api_gateway_s3_api_role.arn
+}
 
-# output "teraform_logging_arn_output" {
-#  value = aws_iam_policy.spain_sub_api_gateway_s3_iam_policy.arn
-# }
+output "role_policy_arn" {
+ value = aws_iam_policy.spain_sub_api_gateway_s3_iam_policy.arn
+}
 
-# # Output the API endpoint
-# output "api_endpoint" {
-#   value = "${aws_api_gateway_deployment.spain_sub_api_gateway_deployment.invoke_url}/contract"
-# }
+output "api_endpoint" {
+  value = "${aws_api_gateway_deployment.spain_sub_api_gateway_deployment.invoke_url}/contract"
+}
