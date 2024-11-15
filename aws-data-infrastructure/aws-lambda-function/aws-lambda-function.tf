80a27a160a08334de5030d701c5455bafef60f73
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-11-15 20:57:05 +0000

    fix key s3 path

--- aws-data-infrastructure/aws-lambda-function/aws-lambda-function.tf
+++ aws-data-infrastructure/aws-lambda-function/aws-lambda-function.tf
@@ -1,80 +1,80 @@
-resource "aws_iam_role" "spain_sub_shopify_flow_api_role" {
-  name = "spain_sub_shopify_flow_api_role"
+# resource "aws_iam_role" "spain_sub_shopify_flow_api_role" {
+#   name = "spain_sub_shopify_flow_api_role"
 
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      },
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "apigateway.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     "Version": "2012-10-17",
+#     "Statement": [
+#       {
+#         "Effect": "Allow",
+#         "Principal": {
+#           "Service": "lambda.amazonaws.com"
+#         },
+#         "Action": "sts:AssumeRole"
+#       },
+#       {
+#         "Effect": "Allow",
+#         "Principal": {
+#           "Service": "apigateway.amazonaws.com"
+#         },
+#         "Action": "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
 
-# IAM policy for logging from a lambda
-resource "aws_iam_policy" "spain_sub_shopify_flow_iam_policy" {
+# # IAM policy for logging from a lambda
+# resource "aws_iam_policy" "spain_sub_shopify_flow_iam_policy" {
 
-  name         = "spain_sub_shopify_flow_iam_policy"
-  path         = "/"
-  description  = "AWS IAM Policy for managing aws lambda role"
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket"
-        ],
-        "Resource": [
-          "arn:aws:s3:::byt-test-dev",
-          "arn:aws:s3:::byt-test-dev/*",
-          "arn:aws:s3:::byt-test-prod",
-          "arn:aws:s3:::byt-test-prod/*"
-        ]
-      },
-      {
-        "Effect": "Allow",
-        "Action": [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "cloudwatch:Get*",
-          "cloudwatch:List*",
-          "cloudwatch:Describe*"
-        ],
-        "Resource": "arn:aws:logs:*:*:*"
-      }
-    ]
-  })
-}
+#   name         = "spain_sub_shopify_flow_iam_policy"
+#   path         = "/"
+#   description  = "AWS IAM Policy for managing aws lambda role"
+#   policy = jsonencode({
+#     "Version": "2012-10-17",
+#     "Statement": [
+#       {
+#         "Effect": "Allow",
+#         "Action": [
+#           "s3:GetObject",
+#           "s3:PutObject",
+#           "s3:ListBucket"
+#         ],
+#         "Resource": [
+#           "arn:aws:s3:::byt-test-dev",
+#           "arn:aws:s3:::byt-test-dev/*",
+#           "arn:aws:s3:::byt-test-prod",
+#           "arn:aws:s3:::byt-test-prod/*"
+#         ]
+#       },
+#       {
+#         "Effect": "Allow",
+#         "Action": [
+#           "logs:CreateLogGroup",
+#           "logs:CreateLogStream",
+#           "logs:PutLogEvents",
+#           "cloudwatch:Get*",
+#           "cloudwatch:List*",
+#           "cloudwatch:Describe*"
+#         ],
+#         "Resource": "arn:aws:logs:*:*:*"
+#       }
+#     ]
+#   })
+# }
 
-# Policy Attachment on the role.
-resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role        = aws_iam_role.spain_sub_shopify_flow_api_role.name
-  policy_arn  = aws_iam_policy.spain_sub_shopify_flow_iam_policy.arn
-}
+# # Policy Attachment on the role.
+# resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+#   role        = aws_iam_role.spain_sub_shopify_flow_api_role.name
+#   policy_arn  = aws_iam_policy.spain_sub_shopify_flow_iam_policy.arn
+# }
 
-# Configure API Gateway account settings to use the CloudWatch Logs role
+# # Configure API Gateway account settings to use the CloudWatch Logs role
 # resource "aws_api_gateway_account" "api_gateway_account" {
 #   cloudwatch_role_arn = aws_iam_role.spain_sub_shopify_flow_api_role.arn
 #   depends_on          = [aws_iam_role.spain_sub_shopify_flow_api_role, aws_iam_policy.spain_sub_shopify_flow_iam_policy]
 # }
 
 
-# Generates an archive from content, a file, or a directory of files.
+# # Generates an archive from content, a file, or a directory of files.
 # data "archive_file" "zip_the_python_code" {
 #  type        = "zip"
 #  source_dir  = "${path.module}/python/"
@@ -82,7 +82,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 # }
 
 
-# # Lambda Function
+# # # Lambda Function
 # resource "aws_lambda_function" "shopify_flow_func" {
 #   filename       = "${path.module}/python/lambda_handler.zip"
 #   source_code_hash = filebase64sha256("${path.module}/python/lambda_handler.zip")
@@ -94,13 +94,13 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 # }
 
 
-# # Get lambda code file from s3.
+# # # Get lambda code file from s3.
 # data "aws_s3_object" "lambda_code_versioned_object" {
 #   bucket = "byt-test-prod"
 #   key    = "spain_sub_lambda_file/spain_sub_function.zip"
 # }
 
-# # Lambda Function
+# # # Lambda Function
 # resource "aws_lambda_function" "shopify_flow_func" {
 #   s3_bucket         = data.aws_s3_object.lambda_code_versioned_object.bucket  
 #   s3_key            = data.aws_s3_object.lambda_code_versioned_object.key
@@ -119,7 +119,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 # }
 
 
-# # REST API Gateway
+# # # REST API Gateway
 # resource "aws_api_gateway_rest_api" "spain_sub_shopify_flow_rest_api" {
 #   name        = "spain_sub_shopify_flow_rest_api"
 #   description = "REST API for Shopify Flow integration"
@@ -133,7 +133,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 #   depends_on  = [aws_api_gateway_rest_api.spain_sub_shopify_flow_rest_api]  
 # }
 
-# # Define GET method on "/subscriptions/contract"
+# # # Define GET method on "/subscriptions/contract"
 # resource "aws_api_gateway_method" "get_contract" {
 #   rest_api_id   = aws_api_gateway_rest_api.spain_sub_shopify_flow_rest_api.id
 #   resource_id   = aws_api_gateway_resource.contract.id
@@ -141,7 +141,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 #   authorization = "NONE"
 # }
 
-# # Define POST method on "/subscriptions/contract"
+# # # Define POST method on "/subscriptions/contract"
 # resource "aws_api_gateway_method" "post_contract" {
 #   rest_api_id   = aws_api_gateway_rest_api.spain_sub_shopify_flow_rest_api.id
 #   resource_id   = aws_api_gateway_resource.contract.id
@@ -149,7 +149,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 #   authorization = "NONE"
 # }
 
-# # Integration for GET method with Lambda
+# # # Integration for GET method with Lambda
 # resource "aws_api_gateway_integration" "lambda_get_integration" {
 #   rest_api_id             = aws_api_gateway_rest_api.spain_sub_shopify_flow_rest_api.id
 #   resource_id             = aws_api_gateway_resource.contract.id
@@ -159,7 +159,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 #   uri                     = aws_lambda_function.shopify_flow_func.invoke_arn
 # }
 
-# # Integration for POST method with Lambda
+# # # Integration for POST method with Lambda
 # resource "aws_api_gateway_integration" "lambda_post_integration" {
 #   rest_api_id             = aws_api_gateway_rest_api.spain_sub_shopify_flow_rest_api.id
 #   resource_id             = aws_api_gateway_resource.contract.id
@@ -169,7 +169,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 #   uri                     = aws_lambda_function.shopify_flow_func.invoke_arn
 # }
 
-# # Lambda Permission for API Gateway to invoke the function
+# # # Lambda Permission for API Gateway to invoke the function
 # resource "aws_lambda_permission" "api_gateway_invoke" {
 #   statement_id  = "AllowAPIGatewayInvoke"
 #   action        = "lambda:InvokeFunction"
@@ -178,7 +178,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 #   source_arn    = "${aws_api_gateway_rest_api.spain_sub_shopify_flow_rest_api.execution_arn}/*"
 # }
 
-# # Deploy the API
+# # # Deploy the API
 # resource "aws_api_gateway_deployment" "spain_sub_shopify_flow_deployment" {
 #   rest_api_id = aws_api_gateway_rest_api.spain_sub_shopify_flow_rest_api.id
 #   depends_on = [
