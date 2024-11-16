54f02402ead46670dabc559a8b7b7db23a7b70a3
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-11-16 18:28:27 +0000

    fixing request_temp

--- aws-data-infrastructure/aws-apigateway-s3/aws-apigateway-s3.tf
+++ aws-data-infrastructure/aws-apigateway-s3/aws-apigateway-s3.tf
@@ -129,8 +129,7 @@ resource "aws_api_gateway_rest_api" "spain_sub_shopify_flow_rest_api" {
 resource "aws_api_gateway_resource" "spain_sub_resource" {
   rest_api_id = aws_api_gateway_rest_api.spain_sub_shopify_flow_rest_api.id
   parent_id   = aws_api_gateway_rest_api.spain_sub_shopify_flow_rest_api.root_resource_id
-#   path_part   = "contract"
-  path_part   = "{dataSource}" # Path parameter for event type
+  path_part   = var.bucket_name     #"{dataSource}" 
   depends_on  = [aws_api_gateway_rest_api.spain_sub_shopify_flow_rest_api]
 }
 
@@ -141,9 +140,9 @@ resource "aws_api_gateway_method" "spain_sub_put_method" {
   http_method   = "POST"
   authorization = "NONE"
   
-  request_parameters = {
-    "method.request.path.dataSource" = true
-  }
+#   request_parameters = {
+#     "method.request.path.dataSource" = true
+#   }
 }
 
 
@@ -152,17 +151,16 @@ resource "aws_api_gateway_integration" "spain_sub_put_integration" {
   rest_api_id             = aws_api_gateway_rest_api.spain_sub_shopify_flow_rest_api.id
   resource_id             = aws_api_gateway_resource.spain_sub_resource.id
   http_method             = aws_api_gateway_method.spain_sub_put_method.http_method
-  integration_http_method = "PUT"  # S3 requires PUT for object creation
+  integration_http_method = "PUT"  
   type                    = "AWS"
   uri                     = "arn:aws:apigateway:${var.region}:s3:path/{bucket}/{key}"
-  # uri                     = "arn:aws:apigateway:${var.region}:s3:path/${data.aws_s3_bucket.spain_sub_event_bucket.bucket}/{key}"
-  # uri                     = "arn:aws:apigateway:${var.region}:s3:path/${data.aws_s3_bucket.spain_sub_event_bucket.bucket}/{foldername}/{filename}"
   credentials             = aws_iam_role.spain_sub_api_gateway_s3_api_role.arn
   passthrough_behavior    = "WHEN_NO_MATCH"
 
-  request_parameters = {
-    "integration.request.path.dataSource" = "method.request.path.dataSource"
-  }
+#   request_parameters = {
+#     "integration.request.path.dataSource" = "method.request.path.dataSource"
+#   }
+# #set($context.requestOverride.path.bucket = "$input.params('dataSource')")
 
   request_templates = {
     "application/json" = <<EOT
@@ -170,7 +168,7 @@ resource "aws_api_gateway_integration" "spain_sub_put_integration" {
 #set($eventType = $input.path('$.event_type'))
 #set($pathName = "bronze")
 #set($key = $pathName + "/" + $eventType + "/" + $eventType + "_" + $timestamp + ".json")
-#set($context.requestOverride.path.bucket = $input.params('dataSource'))
+#set($context.requestOverride.path.bucket = "${var.bucket_name}")
 #set($context.requestOverride.path.key = $key)
 
 {
