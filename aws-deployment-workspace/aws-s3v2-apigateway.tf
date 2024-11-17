922ff0952c1b2d438ab44a3cfb059133f885ee1c
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-11-17 19:54:05 +0000

    fixing request

--- aws-deployment-workspace/aws-s3v2-apigateway.tf
+++ aws-deployment-workspace/aws-s3v2-apigateway.tf
@@ -1,3 +1,3 @@
-module "aws-s3v2-apigateway" {
-  source = "../aws-data-infrastructure/aws-apigateway-s3/"
-}
+# module "aws-s3v2-apigateway" {
+#   source = "../aws-data-infrastructure/aws-apigateway-s3/"
+# }
