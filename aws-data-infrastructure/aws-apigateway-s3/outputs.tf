a6a969de7e71d3364b441fda5083d0816dae858a
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-11-18 12:49:02 +0000

    fixing request

--- aws-data-infrastructure/aws-apigateway-s3/outputs.tf
+++ aws-data-infrastructure/aws-apigateway-s3/outputs.tf
@@ -6,6 +6,6 @@ output "role_apigateway_s3_policy_arn" {
   value = aws_iam_policy.spain_sub_api_gateway_s3_iam_policy.arn
 }
 
-# # output "invoke_url" {
-# #   value = aws_api_gateway_stage.spain_sub_apigateway_stage.invoke_url
-# # }
+output "invoke_url" {
+  value = aws_api_gateway_stage.spain_sub_apigateway_stage.invoke_url
+}
