a7849e2d737197fed16dc8aeaefb6cff70aea17d
rash-ms <166479512+rash-ms@users.noreply.github.com>
2024-11-25 13:54:38 +0000

    Update outputs.tf

--- aws-data-infrastructure/aws-apigateway-s3/outputs.tf
+++ aws-data-infrastructure/aws-apigateway-s3/outputs.tf
@@ -9,3 +9,7 @@
 # output "invoke_url" {
 #   value = aws_api_gateway_stage.spain_sub_apigateway_stage.invoke_url
 # }
+
+output "log_group_prefix" {
+  value = "/aws/apigateway/API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.spain_sub_apigateway_shopify_flow_rest_api.id}"
+}
