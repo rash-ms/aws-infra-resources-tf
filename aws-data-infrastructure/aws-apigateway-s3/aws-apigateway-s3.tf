147a2e3780100116776d49d72cd75262b8bcf9ad
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-11-25 18:31:32 +0000

    add

--- aws-data-infrastructure/aws-apigateway-s3/aws-apigateway-s3.tf
+++ aws-data-infrastructure/aws-apigateway-s3/aws-apigateway-s3.tf
@@ -2,17 +2,12 @@ resource "aws_api_gateway_deployment" "spain_sub_apigateway_s3_deployment" {
 
   # Use triggers to force deployment recreation when the file changes
   triggers = {
-    #redeploy = md5(file("${path.module}/api_gateway.tf"))
     stage_description = md5(file("${path.module}/aws-apigateway-resources.tf"))
   }
 
   # Optional: Add description
   stage_description = "API deployment for stage dev - ${timestamp()}" 
 
-  # stage_description = "${md5(file("aws-apigateway-resources.tf"))}"
-  # stage_description = md5(file("${path.module}/aws-apigateway-resources.tf"))
-
-  # rest_api_id = aws_api_gateway_rest_api.spain_sub_apigateway_shopify_flow_rest_api.id
   rest_api_id = "${aws_api_gateway_rest_api.spain_sub_apigateway_shopify_flow_rest_api.id}"
 
   depends_on = [
