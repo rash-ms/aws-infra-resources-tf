4a62dd7651f0dac79cd1bc7a58ded8a2d972e894
rash-ms <166479512+rash-ms@users.noreply.github.com>
2024-11-25 11:50:49 +0000

    Update api-gateway-deploy.tf

--- aws-data-infrastructure/aws-apigateway-s3/api-gateway-deploy.tf
+++ aws-data-infrastructure/aws-apigateway-s3/api-gateway-deploy.tf
@@ -1,1 +1,14 @@
+resource "aws_api_gateway_deployment" "spain_sub_apigateway_s3_deployment" {
 
+  stage_description = "${md5(file("aws-apigateway-s3.tf"))}"
+  # rest_api_id = aws_api_gateway_rest_api.spain_sub_apigateway_shopify_flow_rest_api.id
+  rest_api_id = "${aws_api_gateway_rest_api.spain_sub_apigateway_shopify_flow_rest_api.id}"
+
+  depends_on = [
+    aws_api_gateway_method.spain_sub_apigateway_create_method,
+    aws_api_gateway_integration.spain_sub_apigateway_s3_integration_request,
+    aws_api_gateway_integration_response.spain_sub_apigateway_s3_integration_response,
+    aws_api_gateway_method_response.spain_sub_apigateway_s3_method_response
+  ]
+
+}
