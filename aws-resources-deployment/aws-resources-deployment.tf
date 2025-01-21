5402e8d8f7950a52dc70db2f1876009a171f73f2
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-01-21 23:37:35 +0000

    push eventbridge-apigateway-firehose

--- aws-resources-deployment/aws-resources-deployment.tf
+++ aws-resources-deployment/aws-resources-deployment.tf
@@ -1,15 +1,15 @@
 module "aws-s3-bucket" {
-  source = "/aws-resources-deployment/aws-s3-bucket/"
+  source = "aws-resources-deployment/aws-s3-bucket/"
 }
 
-module "aws-apigateway-s3" {
-  source = "/aws-resources-deployment/aws-apigateway-s3/"
-}
+# module "aws-apigateway-s3" {
+#   source = "/aws-resources-deployment/aws-apigateway-s3/"
+# }
 
-module "aws-lambda-function" {
-  source = "/aws-resources-deployment/aws-lambda-function/"
-}
+# module "aws-lambda-function" {
+#   source = "/aws-resources-deployment/aws-lambda-function/"
+# }
 
-module "aws-apigateway-eventbridge-firehose-s3" {
-  source = "/aws-resources-deployment/aws-apigateway-eventbridge-firehose-s3/"
-}
+# module "aws-apigateway-eventbridge-firehose-s3" {
+#   source = "/aws-resources-deployment/aws-apigateway-eventbridge-firehose-s3/"
+# }
